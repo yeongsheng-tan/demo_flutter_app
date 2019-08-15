db53806e02bd07d4bf054756586d1af7a213f55c
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2019-08-15 17:41:49 +0800

    update tests to reflect feature implemented

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:demo_flutter_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Search countries in continent smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Flutter GraphQL Client'), findsOneWidget);
+    expect(find.text('Countries of ??'), findsOneWidget);
+    expect(find.text('Antartica'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Tap the 'Search' icon and search for countries in AN.
+    await tester.tap(find.byIcon(Icons.search));
+    await tester.pumpAndSettle();
+    expect(find.text('Search continents: AF, EU, OC ...'), findsOneWidget);
+    await tester.enterText(find.byType(TextField), 'an');
+    await tester.pumpAndSettle();
+    expect(find.text('an'), findsOneWidget);
+    expect(find.text('Countries of AN'), findsOneWidget);
   });
 }
