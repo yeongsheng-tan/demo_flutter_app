e71790fc568b9b5256e8152fb0e64c8119f91999
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2019-11-11 14:47:39 +0800

    upgrade flutter dart and nodejs

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/yeongsheng/.asdf/installs/flutter/1.9.1+hotfix.4-stable"
-export "FLUTTER_APPLICATION_PATH=/Users/yeongsheng/code/demo_flutter_app"
+export "FLUTTER_ROOT=/Users/yeongsheng/.asdf/installs/flutter/1.9.1+hotfix.6-stable"
+export "FLUTTER_APPLICATION_PATH=/Users/yeongsheng/titansoft_code/demo_flutter_app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/yeongsheng/.asdf/installs/flutter/1.9.1+hotfix.4-stable/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yeongsheng/.asdf/installs/flutter/1.9.1+hotfix.6-stable/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
