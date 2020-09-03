49b68f560e521f7e7077e6559e76c81c2ae351a1
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2020-09-03 13:53:53 +0800

    upgrade to flutter 1.20.3-stable

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/yeongsheng/.asdf/installs/flutter/1.20.1-stable"
+export "FLUTTER_ROOT=/Users/yeongsheng/.asdf/installs/flutter/1.20.2-stable"
 export "FLUTTER_APPLICATION_PATH=/Users/yeongsheng/titansoft_code/demo_flutter_app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/yeongsheng/.asdf/installs/flutter/1.20.1-stable/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yeongsheng/.asdf/installs/flutter/1.20.2-stable/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
